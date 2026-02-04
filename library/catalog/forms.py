9ebe187eeeae753ec4b320988a3b2113a3e61a64
Gerry <gerryoiveras@gmail.com>
2026-02-04 14:22:00 +0100

    modificato css e login e view.py

--- library/catalog/forms.py
+++ library/catalog/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 
 class SignupForm(UserCreationForm):
+
     class Meta:
         model = User
-        fields = ['first_name','last_name','email','username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Rimuove TUTTI i testi automatici
+        for field in self.fields.values():
+            field.help_text = None
